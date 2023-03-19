b022fb9d5bde4f8ffbd2f16e3d342ef12051bdae
APIMontero <asia.david.montero@gmail.com>
2023-03-18 23:15:38 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Mi primera aplicación 👜 </h1>
+        <h2>React con TypeScript ‼ </h2>
+        <a href="https://www.google.cl/" className="App-link">
+          Ir a google 🇨🇱
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
